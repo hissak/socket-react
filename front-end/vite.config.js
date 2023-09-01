a5f96ef30e46f464e5d2c3f807832d4a5fa5d489
Hassan Issak <hassanissak@Oghma-Infinium.local>
2023-09-01 15:30:40 -0400

    Initial commit

---
+++ front-end/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    proxy: {
+      '/socket.io/': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+    }
+  },
+  plugins: [react()],
+})
